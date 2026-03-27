23a4e28808927738d56d5c05db79f3a813f59ca0
shriom17 <129147531+shriom17@users.noreply.github.com>
2026-03-27 22:06:44 +0530

    updated

--- app/baseline.py
+++ app/baseline.py
@@ -2,11 +2,13 @@
 def run_baseline(env):
     state = env.reset()
     total_reward = 0
-
     done = False
+
     while not done:
+        
         patient = max(state.patients, key=lambda x: x.severity)
-        action = {"select_patient_id": patient.id}
+
+        action = type("Action", (), {"select_patient_id": patient.id})()
 
         state, reward, done = env.step(action)
         total_reward += reward
